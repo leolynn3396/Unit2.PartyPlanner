e8699b96706a7f7d4279b6ced9c165648487dba3
Ling Huang <linghuang@Lings-MacBook-Pro.local>
2023-11-17 15:18:51 -0800

    final

--- index.js
+++ index.js
@@ -1,120 +1,121 @@
-const API_URL = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2308-acc-pt-web-pt-a/events";
+const EVENTS_URI =
+  "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2308-ACC-PT-WEB-PT-A/events";
 
 const state = {
-    parties: [],
-
+  events: [],
 };
 
-const partiesList = document.querySelector("#parties");
+const partiesList = document.querySelector("#display-parties");
 
-const addPartyForm = document.querySelector("#addParty");
-addPartyForm.addEventListener("Submit", addParty);
-
-async function render() {
-    await getParties();
-    renderParties();
-}
-render();
-
-//update server with parties frmo API
-
-async function getParties(){
-    try {
-        const response = await getch(API_URL);
-        const json = await response.json();
-        state.parties = json.data;
-    } catch (error) {
-        console.error(error);
+//GET EVENTS
+const getParties = async () => {
+  try {
+    const response = await fetch(EVENTS_URI);
+    const json = await response.json();
+    const parties = json.data;
+    console.log("inside GET");
+    console.log(parties);
+    if (json.error) {
+      throw new Error(json.error);
     }
-}
+    state.events = parties;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-//adding Party information
-//names, dates, times, locations, and descriptions of all the parties that are happening.
-
-async function addParty(event) {
-    event.preventDefault();
-
-    await createParty(
-        addPartyForm.title.value,
-        addPartyForm.date.value,
-        addPartyForm.time.value,
-        addPartyForm.location.value,
-        addPartyForm.description.value,
-    );
-}
-
-async function createParty(name, date, time, location, description){
-    try {
-        const response = await fetch(API_URL, {
-            method:"POST",
-            headers:{"Content-Type":"application/json"},
-            body: JSON.stringify({name, imageUrl, description}),
-        });
-        const json = await response.json();
-        if (json.error){
-            throw new Error(json.message);
-        }
-        render();
-    }catch (error) {
-        console.error(error);
+//POST EVENT
+//name, time, location, description
+const createParty = async (name, description, unformattedDate, location) => {
+  try {
+    const date = new Date(unformattedDate);
+    const response = await fetch(EVENTS_URI, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, description, date, location }),
+    });
+    const json = await response.json();
+    if (json.error) {
+      throw new Error(json.error);
     }
+    init();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+document.querySelector("form").addEventListener("submit", (evt) => {
+  const formEl = evt.target;
+  evt.preventDefault();
+  createParty(
+    formEl.title.value,
+    formEl.description.value,
+    formEl.date.value,
+    formEl.location.value
+  );
+
+  // clear inputs
+  formEl.title.value = "";
+  formEl.description.value = "";
+  formEl.date.value = "";
+  formEl.location.value = "";
+
+  // focus the first form element
+  formEl.title.focus();
+});
+
+//DELETE EVENT
+
+const deleteParty = async (id) => {
+  try {
+    const response = await fetch(EVENTS_URI + "/" + id, { method: "DELETE" });
+    const json = response.json();
+    const parties = json.data;
+    if (json.error) {
+      throw new Error(json.error);
     }
+    state.events = parties;
+    init();
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-    async function updateParty(id, date, time, location, description ) {
-        try {
-            const response = await fetch('${API_URL}/${id}',{
-                method: "PUT",
-                headers:{"Content-Type": "application/json"},
-                body: JSON.stringify({date, time, location, description}),
-            });
-            const json = await response.json();
-            
-            if (json.error) {
-                throw new Error(json.message);
-            }
-            render();
-        }catch(error) {
-            console.error(error);
-        }
-    }
+function renderEvents() {
+  console.log("inside renderEvents");
+  console.log(state);
+  if (!state.events || !state.events.length) {
+    partiesElement = document.createElement("div");
+    partiesElement.innerHTML = `<li>No events found</li>`;
+    return;
+  }
 
-    async function deleteParty(id){
-        try{
-            const response = await fetch('${API_URL}/${id}',{
-                method: "DELETE",
-            });
-            if(!response.ok){
-                throw new Error("Party could not be deleted.");
-            }
-            render();
-        }catch(error){
-            console.log(error);
-        }
-    }
-
-    function renderParties() {
-        if (!state.parties.length){
-            partiesList.innerHTML = '<li>No parties found.<.li>';
-            return;
-        }
-
-    const partyCards = state.parties.map((recipe)=>{
-        const partyCard = document.createElement("li");
-        partyCard.classList.add("party");
-        partyCard.innerHTML = '
+  const partyItems = state.events.map((party) => {
+    console.log("inside partyItems");
+    console.log(party);
+    const partyItem = document.createElement("li");
+    partyItem.classList.add("party");
+    partyItem.innerHTML = `
         <h2>${party.name}</h2>
-        <p> ${party.date}</p>
-        <p>${party.time}</p>
+        <p>${party.description}<p>
+        <p>${party.date}</p>
         <p>${party.location}</p>
-        <p>${party.description}</p> 
-        ';
+        <p>${party.id}</p>
+        `;
 
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "Delete Party";
-    partyCard.append(deleteButton);
+    partyItem.append(deleteButton);
 
     deleteButton.addEventListener("click", () => deleteParty(party.id));
-    return partyCard;
-    });
-    partiesList.replaceChildren(...partyCards);
-    }
+    return partyItem;
+  });
+  console.log(partyItems);
+  partiesList.replaceChildren(...partyItems);
+}
+
+const init = async () => {
+  await getParties();
+  renderEvents();
+};
+init();
